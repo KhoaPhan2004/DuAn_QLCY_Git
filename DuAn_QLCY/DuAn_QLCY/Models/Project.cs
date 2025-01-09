cf8104b612713bc03f2b0fb424351fe39c64bfbf
khoa <tkpkpin002@gmail.com>
2025-01-09 23:23:19 +0700

    update code lan 4 ban 1.0

---
+++ DuAn_QLCY/DuAn_QLCY/Models/Project.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DuAn_QLCY.Models;
+
+public partial class Project
+{
+    public int ProjectId { get; set; }
+
+    public string? ProjectName { get; set; }
+
+    public DateOnly? StartDate { get; set; }
+
+    public DateOnly? EndDate { get; set; }
+
+    public string? Status { get; set; }
+
+    public int? ManagerId { get; set; }
+
+    public int? ClientId { get; set; }
+
+    public virtual Client? Client { get; set; }
+
+    public virtual Employee? Manager { get; set; }
+
+    public virtual ICollection<ProjectDetail> ProjectDetails { get; set; } = new List<ProjectDetail>();
+
+    public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+}
---
+++ DuAn_QLCY/DuAn_QLCY/Models/Salary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DuAn_QLCY.Models;
+
+public partial class Salary
+{
+    public int EmployeeId { get; set; }
+
+    public decimal? Salary1 { get; set; }
+
+    public decimal? Bonus { get; set; }
+
+    public DateOnly PaymentDate { get; set; }
+
+    public virtual Employee Employee { get; set; } = null!;
+}
