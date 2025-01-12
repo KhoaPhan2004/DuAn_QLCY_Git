961c2d0f944009100f526e20936f21d98139a54c
khoa <tkpkpin002@gmail.com>
2025-01-12 17:16:26 +0700

    update code Employees CRUD version 1.0

--- DuAn_QLCY/DuAn_QLCY/Controllers/HomeController.cs
+++ DuAn_QLCY/DuAn_QLCY/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using DuAn_QLCY.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using X.PagedList.Extensions;
@@ -27,14 +29,62 @@ namespace DuAn_QLCY.Controllers
         {
             return View();
         }
-
+        public IActionResult DepartmentList(int? page)
+        {
+            var Department = _qtCtPmContext.Departments.ToPagedList(page ?? 1, 10);
+            return View(Department);
+        }
         public IActionResult EmployeesList(int? page)
         {
             var Employees = _qtCtPmContext.Employees.Include(a => a.Department).Include(a => a.Type).ToPagedList(page ?? 1, 10);
             return View(Employees);
         }
+        public IActionResult CreateEmployees()
+        {
+            var departments = _qtCtPmContext.Departments.ToList();
+            var employeeTypes = _qtCtPmContext.EmployeeTypes.ToList();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            ViewBag.DepartmentsSelectList = new SelectList(departments, "DepartmentId", "DepartmentName");
+            ViewBag.EmployeeTypesSelectList = new SelectList(employeeTypes, "TypeId", "TypeName");
+
+            return View();
+        }
+
+        [HttpPost]
+		public IActionResult CreateEmployees(Employee employees)
+		{
+            _qtCtPmContext.Employees.Add(employees);
+                _qtCtPmContext.SaveChanges();
+            
+			return RedirectToAction("EmployeesList");
+		}
+
+        public IActionResult UpdateEmployees(int id)
+        {
+            var employees = _qtCtPmContext.Employees.Where(t => t.EmployeeId == id).FirstOrDefault();
+
+            var departments = _qtCtPmContext.Departments.ToList();
+            var employeeTypes = _qtCtPmContext.EmployeeTypes.ToList();
+            ViewBag.DepartmentsSelectList = new SelectList(departments, "DepartmentId", "DepartmentName");
+            ViewBag.EmployeeTypesSelectList = new SelectList(employeeTypes, "TypeId", "TypeName");
+
+            return View(employees);
+        }
+        [HttpPost]
+        public IActionResult UpdateEmployees(Employee employees)
+        {
+            _qtCtPmContext.Employees.Update(employees);
+            _qtCtPmContext.SaveChanges();
+            return RedirectToAction("EmployeesList");
+        }
+		public IActionResult DeleteEmployees(int id)
+		{
+			var employees = _qtCtPmContext.Employees.Where(t => t.EmployeeId == id).FirstOrDefault();
+			_qtCtPmContext.Remove(employees);
+			_qtCtPmContext.SaveChanges();
+			return RedirectToAction("EmployeesList");
+		}
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- DuAn_QLCY/DuAn_QLCY/obj/Debug/net8.0/DuAn_QLCY.AssemblyInfo.cs
+++ DuAn_QLCY/DuAn_QLCY/obj/Debug/net8.0/DuAn_QLCY.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DuAn_QLCY")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06f50e16944e6cb8b193bbf927bd7cb27dbd44f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8104b612713bc03f2b0fb424351fe39c64bfbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DuAn_QLCY")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DuAn_QLCY")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
